7a517fcc3c634371bd62e3e0050aa2aa0fda6ddb
Jessy TORREZ <jestor.devweb@gmail.com>
2025-09-04 10:40:32 +0200

    feat: :sparkles: Oops

--- components/sections/Home/SkillsSection.tsx
+++ components/sections/Home/SkillsSection.tsx
@@ -1,6 +1,6 @@
 export default function SkillsSection() {
   return (
-    <section className="skills-section py-12 px-6 bg-gray-50">
+    <section className="py-12 px-6 bg-gray-50">
 
     </section>
   );
